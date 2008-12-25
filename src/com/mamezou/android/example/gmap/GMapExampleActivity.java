57183f484e5b795e5f3bdf594f818015afebb75a
koichi <koichi@192.168.1.142>
2008-12-25 18:29:21 +0900

    初期作成

---
+++ src/com/mamezou/android/example/gmap/GMapExampleActivity.java
@@ -0,0 +1,13 @@
+package com.mamezou.android.example.gmap;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GMapExampleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
