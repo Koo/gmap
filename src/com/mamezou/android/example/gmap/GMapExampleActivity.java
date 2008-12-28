305d827e026a7c83ab24b27bb45553b89f59d2e7
asanokouichi <asanokouichi@shiro>
2008-12-28 20:04:36 +0900

    画面表示できるまで

--- src/com/mamezou/android/example/gmap/GMapExampleActivity.java
+++ src/com/mamezou/android/example/gmap/GMapExampleActivity.java
@@ -1,13 +1,19 @@
 package com.mamezou.android.example.gmap;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class GMapExampleActivity extends Activity {
+import com.google.android.maps.MapActivity;
+
+public class GMapExampleActivity extends MapActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return true;
+	}
 }
